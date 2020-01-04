7c60d7e0d5188cad366823d633456fffd876b4e0
C-uo <chouyuou@gmail.com>
2020-01-04 20:35:48 +0900

    none

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+    constructor(porp) {
+        super(porp);
+        this.state = {
+            local: "http://127.0.0.1:5000",
+            aws: "http://52.68.6.85/blogs/ondo",
+            tmp: NaN
+        }
+    }
+
+    componentDidMount() {
+        Axios
+            .get(this.state.aws)
+            .then(response => {
+                console.log(response);
+                this.setState({tmp: response.data})
+            });
+    };
+
+    reload() {
+        setTimeout(() => {
+            Axios
+                .get(this.state.aws)
+                .then(response => {
+                    this.setState({tmp: response.data})
+                });
+        }, 1000);
+    }
+
+    render() {
+        this.reload();
+        return (
+            <div className={"App"}>
+                {this.state.tmp}
+            </div>
+        )
+    }
+
 }
 
-export default App;
